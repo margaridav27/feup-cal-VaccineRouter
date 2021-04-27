975273a65189c13d614c464d64c7f77273f7d879
Margarida Vieira <margaridajcv.27@gmail.com>
2021-04-27 12:43:39 +0100

    refactored Edge constructor

--- src/VaccineRouter/graph/edge/Edge.cpp
+++ src/VaccineRouter/graph/edge/Edge.cpp
@@ -4,6 +4,9 @@ template<class T>
 Edge<T>::Edge() {}
 
 template<class T>
-Edge<T>::Edge(unsigned int id, double w, Node<T> *dest) : id(id), w(w), dest(dest) {}
+Edge<T>::Edge(unsigned int id, double w, Node<T> *dest) :
+    id(id),
+    w(w),
+    dest(dest) {}
 
 

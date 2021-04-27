44a2447532ed61ebb87235baa406651fce390964
Margarida Vieira <margaridajcv.27@gmail.com>
2021-04-27 11:04:40 +0100

    setup tests for tarjan's algorithm

---
+++ tests/TarjanAlgorithmTest/cmake-build-debug/CMakeFiles/TarjanAlgorithmTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TarjanAlgorithmTest.
+# This may be replaced when dependencies are built.
---
+++ tests/TarjanAlgorithmTest/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "../../src/VaccineRouter/graph/Graph.h"
+#include "../../src/VaccineRouter/algorithms/Tarjan.h"
+
+int main() {
+    // TODO make tests
+}

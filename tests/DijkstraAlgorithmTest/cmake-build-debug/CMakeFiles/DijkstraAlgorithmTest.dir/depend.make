6d1b29b13ab8dbe766b33c52a06016f6ad49393b
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-10 00:27:20 +0100

    deleted useless files

--- tests/DijkstraAlgorithmTest/cmake-build-debug/CMakeFiles/DijkstraAlgorithmTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DijkstraAlgorithmTest.
-# This may be replaced when dependencies are built.
--- tests/TarjanAlgorithmTest/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "../../src/VaccineRouter/graph/Graph.h"
-#include "../../src/VaccineRouter/algorithms/Tarjan.h"
-
-int main() {
-    // TODO make tests
-}

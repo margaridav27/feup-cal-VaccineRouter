60b9349cd9ad898877915d6faf0a7d935ca58f12
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-13 15:09:52 +0100

    started on graphProcessor

--- src/VaccineRouter/graph/GraphProcessor.cpp
+++ src/VaccineRouter/graph/GraphProcessor.cpp
@@ -1,1 +1,19 @@
 #include "GraphProcessor.h"
+
+Graph * processGraph(std::string chosenCity) {
+    Graph *graph = new Graph();
+
+    processNodes(graph, chosenCity);
+    processEdges(graph, chosenCity);
+
+    return graph;
+}
+
+void processEdges(Graph *graph, std::string chosenCity) {
+    std::string nodesPath = R"(..\..\..\cityMaps\)" + chosenCity + R"("\")" + chosenCity + "_strong_nodes_xy.txt";
+}
+
+void processNodes(Graph *graph, std::string chosenCity) {
+    std::string edgesPath = R"(..\..\..\cityMaps\)" + chosenCity + R"("\")" + chosenCity + "_strong_edges.txt";
+
+}
--- src/VaccineRouter/graph/GraphProcessor.h
+++ src/VaccineRouter/graph/GraphProcessor.h
@@ -1,10 +1,13 @@
 #ifndef VACCINEROUTER_GRAPHPROCESSOR_H
 #define VACCINEROUTER_GRAPHPROCESSOR_H
 
-// TODO once I know how the information comes from OpenStreetMaps
-class GraphProcessor {
+#include <string>
+#include "Graph.h"
 
-};
+Graph * processGraph(std::string chosenCity);
 
+void processEdges(Graph *graph, std::string chosenCity);
+
+void processNodes(Graph *graph, std::string chosenCity);
 
 #endif //VACCINEROUTER_GRAPHPROCESSOR_H

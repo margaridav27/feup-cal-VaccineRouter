28b7cbc9ce849cf2ff87e726b7edc4fb35656c00
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-18 10:25:07 +0100

    new time constructor

--- src/VaccineRouter/utilities/Time.h
+++ src/VaccineRouter/utilities/Time.h
@@ -6,20 +6,18 @@
 
 class Time {
 private:
-  unsigned int hour;
-  unsigned int minute;
-  unsigned int second;
+    unsigned int hour;
+    unsigned int minute;
+    unsigned int second;
 public:
-  Time(unsigned int hour, unsigned int minute, unsigned int second);
-  Time(std::string timeStr);
-
-  Time *getTime();
-  void setTime(unsigned int hour, unsigned  int minute, unsigned int second);
-  void setTime(std::string timeStr);
-
-  Time *operator+(Time t) const;
-  std::ostream &operator<<(std::ostream &o) const;
-
-
+    Time(unsigned int hour, unsigned int minute, unsigned int second);
+    Time(std::string timeStr);
+    Time(double hours);
+    Time *getTime();
+    void setTime(unsigned int hour, unsigned int minute, unsigned int second);
+    void setTime(std::string timeStr);
+    Time *operator+(Time t) const;
+    std::ostream &operator<<(std::ostream &o) const;
 };
+
 #endif //VACCINEROUTER_TIME_H

0ed51e61a2cc13ab95160523b798b83cfb87d741
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-22 15:20:41 +0100

    spaces

--- src/VaccineRouter/ui/Interface.h
+++ src/VaccineRouter/ui/Interface.h
@@ -9,10 +9,13 @@ private:
     bool checkInRange(int optionsRange, int input);
     bool checkGeneralInputValidity(int optionsRange, int input);
     std::vector<int> checkACSelectionValidity(bool multiple, int optionsRange);
+
     std::vector<std::pair<int, std::string>> getAvailableACs(const std::string &mapFilename);
     std::vector<std::pair<int, std::string>> getAvailableSCs(const std::string &mapFilename);
+
     void displayAvailableACs(std::vector<std::pair<int, std::string>> &options);
     void displayAvailableSCs(std::vector<std::pair<int, std::string>> &options);
+
     void initialMenu();
     void runProgramMenu();
     void modifyDataMenu();
@@ -23,6 +26,7 @@ private:
     void selectSingleACMenu(const std::string &mapFilename);
     void selectMultipleACMenu(const std::string &mapFilename);
     void orderVaccinesMenu(std::vector<std::pair<int, std::string>> &options, const std::vector<int> &selected);
+
     void singleAC();
     void multipleACWithTW();
     void multipleAC();
